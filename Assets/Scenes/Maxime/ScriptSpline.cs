ad66cfd5127fb5c6c2ba79c4d9a0c1ed5e9d3d14
Maxime Fortier <202163543@edu.clg.qc.ca>
2023-02-15 12:52:37 -0500

    Mesh pour la spline de la piste

---
+++ Assets/Scenes/Maxime/ScriptSpline.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptSpline : MonoBehaviour
+{
+    public float largeurRoute = 7.5f;
+
+    Mesh CréerMeshRoute(List<Vector3> pointsSpline, bool estFermé) // prend vector3 pour faire route et s'assure de fermer la route
+    {
+        Vector3[] sommets = new Vector3[pointsSpline.Count * 2];
+        int nbTriangles = 2 * (pointsSpline.Count - 1) + ((estFermé) ? 2 : 0);
+        int[] triangle = new int[nbTriangles * 3];
+        int indexSom = 0;
+        int indexTri = 0;
+
+        for (int i = 0; i < pointsSpline.Count; i++) //pour chaque point on veut la direction forward et 2 sommets/point
+        {
+            Vector3 directionAvant = Vector3.zero;
+            if (i < pointsSpline.Count - 1 || estFermé) // dernier point
+            {
+                directionAvant += pointsSpline[(i + 1) % pointsSpline.Count] - pointsSpline[i];
+            }
+            if (i > 0 || estFermé) // premier point
+            {
+                directionAvant += pointsSpline[i] - pointsSpline[(i - 1 + pointsSpline.Count) % pointsSpline.Count];
+            }
+            directionAvant.Normalize();
+            Vector3 gauche = new Vector3(-directionAvant.y, directionAvant.x); //pour prendre sommet à coter du pt
+
+            // calculer 2 sommets
+            sommets[indexSom] = pointsSpline[i] + gauche * largeurRoute * .5f;
+            sommets[indexSom + 1] = pointsSpline[i] - gauche * largeurRoute * .5f;
+
+            //tous les pts sauf le dernier pour faire les triangles 
+            if (i < pointsSpline.Count - 1 || estFermé)
+            {
+                triangle[indexTri] = indexSom;
+                triangle[indexTri + 1] = (indexSom + 2) % sommets.Length;
+                triangle[indexTri + 2] = indexSom + 1;
+
+                triangle[indexTri + 3] = indexSom + 1;
+                triangle[indexTri + 4] = (indexSom + 2) % sommets.Length;
+                triangle[indexTri + 5] = (indexSom + 3) % sommets.Length;
+            }
+            indexSom += 2;
+            indexTri += 6;
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = sommets;
+        mesh.triangles = triangle;
+
+        return mesh;
+    }
+}
