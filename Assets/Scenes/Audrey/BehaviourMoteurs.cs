fe35f304894a7706948d8103cd199b8d39f5307e
Audrey Gingras <audreygingras485@gmail.com>
2023-02-15 13:54:33 -0500

    bshdbeubci

--- Assets/Scenes/Audrey/BehaviourMoteurs.cs
+++ Assets/Scenes/Audrey/BehaviourMoteurs.cs
@@ -5,5 +5,11 @@ using UnityEngine;
 //enum?
 public interface Moteurs
 {
+    enum Type
+    {
+        Débutant, // je me rappelle plus c'est quoi le premier
+        Avancé,
+        Pro
+    }
     public int Puissance { get; set; }
 }
