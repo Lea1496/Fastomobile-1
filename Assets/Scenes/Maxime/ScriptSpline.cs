56a8215826412e26722f930d061831f3b1c3a6ca
Maxime Fortier <202163543@edu.clg.qc.ca>
2023-02-20 13:06:06 -0500

    mesh road couleur bizarre

--- Assets/Scenes/Maxime/ScriptSpline.cs
+++ Assets/Scenes/Maxime/ScriptSpline.cs
@@ -9,13 +9,17 @@ using UnityEngine;
 [RequireComponent(typeof(MeshCollider))]
 public class ScriptSpline : MonoBehaviour
 {
-    private float largeurRoute = 75f;
+    private float largeurRoute = 70f;
+    private float tiling = 1;
     private List<Vector3> pointsSpline;
     private Mesh maillage;
     Vector3[] sommets;
     Vector3 gauche;
     Vector3 directionAvant;
-    
+
+    [SerializeField]
+    Material matériaux;
+
     private void Start()
     {
         maillage = new Mesh();
@@ -24,10 +28,15 @@ public class ScriptSpline : MonoBehaviour
         maillage = CréerMeshRoute(pointsSpline);
         GetComponent<MeshFilter>().mesh = maillage;
         meshc.sharedMesh = maillage;
+        GetComponent<MeshRenderer>().material = matériaux;
+
+        int textureRepeat = Mathf.RoundToInt(tiling * pointsSpline.Count * 0.5f); // enlever .5
+        GetComponent<MeshRenderer>().sharedMaterial.mainTextureScale = new Vector2(1, textureRepeat);
     }
     private Mesh CréerMeshRoute(List<Vector3> pointsSpline) // prend vector3 pour faire route et s'assure de fermer la route
     {
         sommets = new Vector3[pointsSpline.Count * 2];
+        Vector2[] uvs = new Vector2[sommets.Length]; // texture route
         int nbTriangles = 2 * (pointsSpline.Count - 1) + 2;
         int[] triangle = new int[nbTriangles * 3];
         int indexSom = 0;
@@ -38,11 +47,11 @@ public class ScriptSpline : MonoBehaviour
             directionAvant = Vector3.zero;
             if (i < pointsSpline.Count - 1) // dernier point 
             {
-                directionAvant += pointsSpline[i + 1] - pointsSpline[i];
+                directionAvant += pointsSpline[(i + 1) % pointsSpline.Count] - pointsSpline[i];
             }
             if (i > 0) // premier point
             {
-                directionAvant += pointsSpline[i] - pointsSpline[i - 1];
+                directionAvant += pointsSpline[i] - pointsSpline[(i - 1 + pointsSpline.Count) % pointsSpline.Count];
             }
             directionAvant.Normalize();
             gauche = new Vector3(-directionAvant.z, 0, directionAvant.x); //pour prendre sommet à coter du pt
@@ -51,17 +60,24 @@ public class ScriptSpline : MonoBehaviour
             sommets[indexSom] = pointsSpline[i] + gauche * largeurRoute;
             sommets[indexSom + 1] = pointsSpline[i] - gauche * largeurRoute;
 
+            //texture
+
+            float completePercent = i / (float)(pointsSpline.Count - 1);
+            float v = 1 - Mathf.Abs(2 * completePercent - 1);
+            uvs[indexSom] = new Vector2(0, v);
+            uvs[indexSom + 1] = new Vector2(1, v);
+
             
             //tous les pts sauf le dernier pour faire les triangles 
             if (i < pointsSpline.Count - 1)
             {
                 triangle[indexTri] = indexSom;
-                triangle[indexTri + 1] = (indexSom + 2);
+                triangle[indexTri + 1] = (indexSom + 2) % sommets.Length;
                 triangle[indexTri + 2] = indexSom + 1;
 
-                triangle[indexTri + 3] = indexSom + 2;
-                triangle[indexTri + 4] = (indexSom + 3);
-                triangle[indexTri + 5] = (indexSom + 1);
+                triangle[indexTri + 3] = (indexSom + 1) ; //2
+                triangle[indexTri + 4] = (indexSom + 2) % sommets.Length; //3
+                triangle[indexTri + 5] = (indexSom + 3) % sommets.Length; //1
             }
             indexSom += 2;
             indexTri += 6;
@@ -75,7 +91,9 @@ public class ScriptSpline : MonoBehaviour
         Mesh mesh = new Mesh();
         mesh.vertices = sommets;
         mesh.triangles = triangle;
-
+        mesh.uv = uvs;
         return mesh;
+
+        // mettre bord sur chaque bord
     }
 }
