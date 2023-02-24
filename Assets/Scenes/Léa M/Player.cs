0b33ccf1bc44570387d2052cb57b3598415bc405
Léa Mongeau <202127854@edu.clg.qc.ca>
2023-02-23 19:47:48 -0500

    Toggles liés (moteur et voiture)

--- Assets/Scenes/Léa M/Player.cs
+++ Assets/Scenes/Léa M/Player.cs
@@ -1,13 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : IPlayer
 {
-    public int Vie { get; }
-    public string Nom { get; }
-    public int Argent { get; }
-    public int Puissance { get; }
-    public int IdVéhicule { get; }
-    public int IdMoteur { get; }
+    
+    public int Vie { get; set; }
+    public string Nom { get; set; }
+    public int Argent { get; private set; }
+    public int IdVéhicule { get; set; }
+    public int IdMoteur { get; set; }
+    public GameObject Chassis { get; set; }
+    
+    public Player()
+    {
+        Chassis.tag = Nom;
+    }
+
+    public void AjouterVie(int vieAjoutée)
+    {
+        GameData.P1.Vie += vieAjoutée;
+    }
+    public void EnleverVie(int vieEnlevée)
+    {
+        GameData.P1.Vie += vieEnlevée;
+    }
+
+    public void Acheter(int prix)
+    {
+        bool peutAcheter = false;
+        if (GameData.P1.Argent >= prix)
+        {
+            GameData.P1.Argent -= prix;
+            peutAcheter = true;
+            
+        }
+    }
+    
+
 }
