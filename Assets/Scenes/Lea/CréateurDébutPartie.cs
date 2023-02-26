5a5c48092eb93b0018a67ed7f05cfe412000cffc
Lea <lea.roy@outook.com>
2023-02-26 16:31:38 -0500

    Camera + Countdown

--- Assets/Scenes/Lea/CréateurDébutPartie.cs
+++ Assets/Scenes/Lea/CréateurDébutPartie.cs
@@ -11,19 +11,23 @@ public class CréateurDébutPartie : MonoBehaviour
     private Vector3 position;
     private int compteurAutos = 0;
     private Vector3 posPremier;
+    private GameObject mainPlayer1;
+
+    public GameObject MainPlayer1
+    {
+        get => mainPlayer1;
+    }
     public CréateurDébutPartie(List<Player> autos, GameObject arc, GameObject ligne, Vector3 pos) //behavior auto pour le moment mais surement à changer
     {
+        
         posPremier =  new Vector3(335, 0, -50);
         lesAutos = new List<GameObject>();
         joueurs = autos;
         for (int i = 0; i < autos.Count; i++)
         {
             lesAutos.Add(autos[i].Chassis);
+            Debug.Log(autos[i].Chassis.name);
         }
-
-       
-
-        
         position = pos;
         Instantiate(arc, new Vector3(position.x + 30, 0, position.z), arc.transform.rotation);
         Instantiate(ligne, new Vector3(position.x + 30, 0, position.z), ligne.transform.rotation);
@@ -33,22 +37,23 @@ public class CréateurDébutPartie : MonoBehaviour
 
     private void InstancierAutos()
     {
-      
+      Debug.Log(lesAutos.Count);
         for (int i = 0; i < lesAutos.Count / 3; i++)
         {
             for (int j = 0; j < lesAutos.Count / 4; j++)
             {
                 GameObject joueur = Instantiate(lesAutos[compteurAutos],
                     new Vector3(position.x - 35 * j - 4 * i, 0, (position.z - 37) + 32 * i - 6 * j),
-                    lesAutos[compteurAutos++].transform.rotation);
+                    lesAutos[compteurAutos].transform.rotation);
                 joueur.GetComponent<Player>().CréerPlayer(
                     joueurs[compteurAutos].Vie, joueurs[compteurAutos].Nom,
                     joueurs[compteurAutos].IdVéhicule, joueurs[compteurAutos].IdMoteur,
                     joueurs[compteurAutos].Chassis, joueurs[compteurAutos].Puissance,
-                    joueurs[compteurAutos].Poid);
-                if (compteurAutos++ == 0)
+                    joueurs[compteurAutos++].Poid);
+                if (compteurAutos - 1 == 0)
                 {
-                    GetComponentInParent<Camera>().transform.parent = joueur.transform;
+                    mainPlayer1 = joueur;
+                   // FindObjectOfType<Camera>().transform.LookAt(joueur.transform); // hummm
                 }
             }
 
