78b548ae67f99c9f21f6da4673d16c64e22aa559
Audrey Gingras <audreygingras485@gmail.com>
2023-03-02 13:42:48 -0500

    Behaviour auto fini (I hope)

--- Assets/Scenes/Audrey/Behaviourauto.cs
+++ Assets/Scenes/Audrey/Behaviourauto.cs
@@ -13,12 +13,12 @@ public class BehaviourAuto : MonoBehaviour/*GestionnairePlayer*/
 
     private int Poids;
     private int Puissance;
-   
+    const int Moto = 2;   
 
     void Start()
     {
-        Poids = GetComponent<Player>().Poids;
-        Puissance = GetComponent<Player>().Puissance;
+        Poids = GameData.P1.GetComponent<Player>().Poids;
+        Puissance = GameData.P1.GetComponent<Player>().Puissance;
     }
 
     private float currentSteerAngle;
@@ -27,58 +27,104 @@ public class BehaviourAuto : MonoBehaviour/*GestionnairePlayer*/
     public bool isBreaking;
     public bool isAccelerating;
 
-    [SerializeField] private float accelerationForce; // reste a déterminer
-    [SerializeField] private float breakForce;
-    [SerializeField] private float maxSteerAngle;
+    [SerializeField] private float accelerationForce; // reste à déterminer
+    [SerializeField] private float breakForce; 
+    [SerializeField] private float maxSteerAngle; 
 
     [SerializeField] private WheelCollider frontLeftWheelCollider;
     [SerializeField] private WheelCollider frontRightWheelCollider;
     [SerializeField] private WheelCollider rearLeftWheelCollider;
     [SerializeField] private WheelCollider rearRightWheelCollider;
 
+    [SerializeField] private WheelCollider frontWheelCollider;
+    [SerializeField] private WheelCollider rearWheelCollider;
+
     [SerializeField] private Transform frontLeftWheelTransform;
-    [SerializeField] private Transform frontRightWheeTransform;
+    [SerializeField] private Transform frontRightWheelTransform;
     [SerializeField] private Transform rearLeftWheelTransform;
     [SerializeField] private Transform rearRightWheelTransform;
 
+    [SerializeField] private Transform frontWheelTransfrom;
+    [SerializeField] private Transform rearWheelTransfrom;
 
     public void HandleMotor(float verticalI)
     {
-        frontLeftWheelCollider.motorTorque = verticalI * Puissance;
-        frontRightWheelCollider.motorTorque = verticalI * Puissance;
-        currentbreakForce = isBreaking ? breakForce : 0f;
-        ApplyBreaking();
-        currentAcceleration = isAccelerating ? accelerationForce : 0f;
-        ApplyAcceleration(verticalI);
+        if(GameData.P1.IdVéhicule != Moto)
+        {
+            frontLeftWheelCollider.motorTorque = verticalI * Puissance;
+            frontRightWheelCollider.motorTorque = verticalI * Puissance;
+            currentbreakForce = isBreaking ? breakForce : 0f;
+            ApplyBreaking();
+            currentAcceleration = isAccelerating ? accelerationForce/Poids : 0f; // parce F=m*a
+            ApplyAcceleration(verticalI);
+        }
+        else
+        {
+            frontWheelCollider.motorTorque = verticalI * Puissance;
+            currentbreakForce = isBreaking ? breakForce : 0f;
+            ApplyBreaking();
+            currentAcceleration = isAccelerating ? accelerationForce/Poids : 0f;
+            ApplyAcceleration(verticalI);
+        }
     }
 
     private void ApplyBreaking()
     {
-        frontRightWheelCollider.brakeTorque = currentbreakForce;
-        frontLeftWheelCollider.brakeTorque = currentbreakForce;
-        rearLeftWheelCollider.brakeTorque = currentbreakForce;
-        rearRightWheelCollider.brakeTorque = currentbreakForce;
+        if (GameData.P1.IdVéhicule != Moto)
+        {
+            frontRightWheelCollider.brakeTorque = currentbreakForce;
+            frontLeftWheelCollider.brakeTorque = currentbreakForce;
+            rearLeftWheelCollider.brakeTorque = currentbreakForce;
+            rearRightWheelCollider.brakeTorque = currentbreakForce;
+        }
+        else
+        {
+            frontWheelCollider.brakeTorque = currentbreakForce;
+            rearWheelCollider.brakeTorque = currentbreakForce;
+        }
     }
 
     private void ApplyAcceleration(float verticalI)
     {
-        frontRightWheelCollider.motorTorque =  verticalI * Puissance * currentAcceleration;
-        frontLeftWheelCollider.motorTorque = verticalI * Puissance * currentAcceleration;
+        if (GameData.P1.IdVéhicule != Moto)
+        {
+            frontRightWheelCollider.motorTorque = verticalI * Puissance * currentAcceleration;
+            frontLeftWheelCollider.motorTorque = verticalI * Puissance * currentAcceleration;
+        }
+        else
+        {
+            frontWheelCollider.brakeTorque = verticalI * Puissance * currentAcceleration;
+        }
     }
 
     public void HandleSteering(float horizontalI)
     {
         currentSteerAngle = maxSteerAngle * horizontalI;
-        frontLeftWheelCollider.steerAngle = currentSteerAngle;
-        frontRightWheelCollider.steerAngle = currentSteerAngle;
+        if(GameData.P1.IdVéhicule != Moto)
+        {
+            frontLeftWheelCollider.steerAngle = currentSteerAngle;
+            frontRightWheelCollider.steerAngle = currentSteerAngle;
+        }
+        else
+        {
+            frontWheelCollider.steerAngle = currentSteerAngle;
+        }  
     }
 
     public void UpdateWheels()
     {
-        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
-        UpdateSingleWheel(frontRightWheelCollider, frontRightWheeTransform);
-        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
-        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+        if(GameData.P1.IdVéhicule != Moto)
+        {
+            UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+            UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+            UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+            UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+        }
+        else
+        {
+            UpdateSingleWheel(frontWheelCollider, frontWheelTransfrom);
+            UpdateSingleWheel(rearWheelCollider, rearWheelTransfrom);
+        }       
     }
 
     public void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
