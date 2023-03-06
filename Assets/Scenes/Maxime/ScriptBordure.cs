65fbd3a9c236b521b8e1b87bb84796339d078503
Maxime Fortier <202163543@edu.clg.qc.ca>
2023-03-06 18:33:36 -0500

    Script bordure pas terminé

--- Assets/Scenes/Maxime/ScriptBordure.cs
+++ Assets/Scenes/Maxime/ScriptBordure.cs
@@ -1,76 +1,107 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using Unity.VisualScripting;
+//using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter))]
-[RequireComponent(typeof(MeshRenderer))]
-[RequireComponent(typeof(MeshCollider))]
-public class ScriptBordure : MonoBehaviour 
-{
-    private Mesh maillage;
-   
-    [SerializeField]
-    Material matériauxBord;
+//[RequireComponent(typeof(MeshFilter))]
+//[RequireComponent(typeof(MeshRenderer))]
+//[RequireComponent(typeof(MeshCollider))]
+//public class ScriptBordure : MonoBehaviour
+//{
+//    private Mesh maillage;
+//    private Vector3[] sommet;
 
-    private void Start()
-    {
-       
-        //maillage = new Mesh();
-        //MeshCollider meshc = gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
-        //maillage = AjouterBordure(sommets);
-        //GetComponent<MeshFilter>().mesh = maillage;
-        //meshc.sharedMesh = maillage;
-        //GetComponent<MeshRenderer>().material = matériauxBord;
+//    [SerializeField]
+//    Material matériauxBord;
 
-        //int textureRepeat = Mathf.RoundToInt(tiling * pointsSpline.Count); 
-        //GetComponent<MeshRenderer>().sharedMaterial.mainTextureScale = new Vector2(1, textureRepeat);
-    }
+//    private void Start()
+//    {
+//        System.Threading.Thread.Sleep(1000);
+//        //Vector3[] sommet = GetComponentInParent<ScriptSpline>().sommets;
+//        List<Vector3> pointsSpline2 = GetComponentInParent<GestionnaireJeux>().Chemin;
+//        Debug.Log(pointsSpline2.Count);
+//        maillage = new Mesh();
+//        MeshCollider meshc = gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
+//        maillage = AjouterBordure(pointsSpline2);
+//        GetComponent<MeshFilter>().mesh = maillage;
+//        meshc.sharedMesh = maillage;
+//        GetComponent<MeshRenderer>().material = matériauxBord;
+
+//        //int textureRepeat = Mathf.RoundToInt(tiling * pointsSpline.Count); 
+//        //GetComponent<MeshRenderer>().sharedMaterial.mainTextureScale = new Vector2(1, textureRepeat);
+//    }
 
     
-    private Mesh AjouterBordure(Vector3[] sommets)
-    {
-
-        Vector2[] uvs = new Vector2[sommets.Length]; // texture route
-        int nbTrianglesPair = sommets.Length;
-        int[] trianglePair = new int[nbTrianglesPair * 3];
-        int indexSom = 0;
-        int indexTri = 0;
-
-        static bool EstPair(int nb) { return nb % 2 == 0; }
-        Vector3[] pointsBordurePair = new Vector3[sommets.Length];
-        //Vector3[] pointsBordureImpair = new Vector3[sommets.Length];
 
 
-        for (int i = 0; i < sommets.Length; i++)
-        {
-            if (EstPair(i))
-            {
-                pointsBordurePair[i] = new Vector3(sommets[i].x, 0, sommets[i].z);
-                pointsBordurePair[i + 1] = new Vector3(sommets[i].x, sommets[i].y + 5f, sommets[i].z);
-            }
+//    private Mesh AjouterBordure(List<Vector3> pointsSpline2)
+//    {
+//        sommet = new Vector3[pointsSpline2.Count * 2];
+//        int indexSomPair = 0;
+//        Vector2[] uvsBordurePair = new Vector2[sommet.Length]; 
+//        int nbTrianglesPair = sommet.Length;
+//        int[] trianglePair = new int[nbTrianglesPair * 3];
+//        int indexTriPair = 0;
+//        static bool EstPair(int nb) { return nb % 2 == 0; }
+//        Vector3[] pointsBordurePair = new Vector3[sommet.Length];
+//        int indexSom = 0;
+//        //Vector3[] pointsBordureImpair = new Vector3[sommet.Length];
 
-            float completePercent = i / (float)(sommets.Length - 1);
-            uvs[indexSom] = new Vector2(0, completePercent);
-            uvs[indexSom + 1] = new Vector2(1, completePercent);
+//        for (int i = 0; i < pointsSpline2.Count; i++)
+//        {
+//            Vector3 directionAvant2 = new Vector3();
+//            directionAvant2 = Vector3.zero;
+//            if (i < pointsSpline2.Count - 1)
+//            {
+//                directionAvant2 += pointsSpline2[(i + 1) % pointsSpline2.Count] - pointsSpline2[i];
+//            }
+//            if (i > 0)
+//            {
+//                directionAvant2 += pointsSpline2[i] - pointsSpline2[(i - 1 + pointsSpline2.Count) % pointsSpline2.Count];
+//            }
+//            directionAvant2.Normalize();
+//            Vector3 gauche = new Vector3(-directionAvant2.z, 0, directionAvant2.x);
 
-            if (i < sommets.Length - 1)
-            {
-                trianglePair[indexTri] = indexSom;
-                trianglePair[indexTri + 1] = (indexSom + 2) % pointsBordurePair.Length;
-                trianglePair[indexTri + 2] = indexSom + 1;
+//            // calculer 2 sommets
+//            sommet[indexSomPair] = pointsSpline2[i] + gauche * 70f;
+//            sommet[indexSomPair + 1] = pointsSpline2[i] - gauche * 70f;
+//            indexSom += 2;
+//        }
+//        Debug.Log(sommet.Length);
 
-                trianglePair[indexTri + 3] = (indexSom + 1);
-                trianglePair[indexTri + 4] = (indexSom + 2) % pointsBordurePair.Length;
-                trianglePair[indexTri + 5] = (indexSom + 3) % pointsBordurePair.Length;
-            }
-            indexSom += 2;
-            indexTri += 6;
-        }
+//        // bordure pair
+//        for (int i = 0; i < sommet.Length; i++)
+//        {
+//            if (EstPair(i))
+//            {
+//                pointsBordurePair[i] = new Vector3(sommet[i].x, 0, sommet[i].z);
+//                pointsBordurePair[i + 1] = new Vector3(sommet[i].x, sommet[i].y + 5f, sommet[i].z);
 
-        Mesh mesh = new Mesh();
-        mesh.vertices = pointsBordurePair;
-        mesh.triangles = trianglePair;
-        mesh.uv = uvs;
-        return mesh;
-    }
-}
+//                float completePercent = i / (float)(sommet.Length - 1);
+//                uvsBordurePair[indexSomPair] = new Vector2(0, completePercent);
+//                uvsBordurePair[indexSomPair + 1] = new Vector2(1, completePercent);
+
+//                if (i < sommet.Length - 1)
+//                {
+//                    trianglePair[indexTriPair] = indexSomPair;
+//                    trianglePair[indexTriPair + 1] = (indexSomPair + 2) % pointsBordurePair.Length;
+//                    trianglePair[indexTriPair + 2] = indexSomPair + 1;
+
+//                    trianglePair[indexTriPair + 3] = (indexSomPair + 1);
+//                    trianglePair[indexTriPair + 4] = (indexSomPair + 2) % pointsBordurePair.Length;
+//                    trianglePair[indexTriPair + 5] = (indexSomPair + 3) % pointsBordurePair.Length;
+//                }
+//                indexSomPair += 2;
+//                indexTriPair += 6;
+
+//            }
+
+//        }
+
+//        Mesh mesh = new Mesh();
+//        mesh.vertices = pointsBordurePair;
+//        mesh.triangles = trianglePair;
+//        mesh.uv = uvsBordurePair;
+//        return mesh;
+//    }
+//}
--- Assets/Scenes/Maxime/ScriptSpline.cs
+++ Assets/Scenes/Maxime/ScriptSpline.cs
@@ -2,8 +2,11 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
@@ -14,12 +17,22 @@ public class ScriptSpline : MonoBehaviour
     //private float tiling = 1;
     private List<Vector3> pointsSpline;
     private Mesh maillage;
+
+    //private Mesh maillageBord;
+    //private Mesh maillageBordImp;
+
     public Vector3[] sommets;
     Vector3 gauche;
     Vector3 directionAvant;
 
     [SerializeField]
     Material matériaux;
+
+    //[SerializeField]
+    //Material matériauxBord;
+
+    //[SerializeField]
+    //Material matériauxBordImp;
 
     private void Start()
     {
@@ -30,33 +43,56 @@ public class ScriptSpline : MonoBehaviour
         GetComponent<MeshFilter>().mesh = maillage;
         meshc.sharedMesh = maillage;
         GetComponent<MeshRenderer>().material = matériaux;
+
+
+
+        //mesh bordure pair
+       // maillage.SetSubMesh(0, maillageBord, [MeshUpdateFlags flags = MeshUpdateFlags.Default]);
         
+        //MeshCollider meshcBord = gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
+        //maillageBord = CréerBordurePair(sommets);
+        //GetComponent<MeshFilter>().mesh = maillageBord;
+        //meshcBord.sharedMesh = maillageBord;
+        //GetComponent<MeshRenderer>().material = matériauxBord;
+
+        //mesh bordure impair
+        //maillageBordImp = new Mesh();
+        //MeshCollider meshcBordImp = gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
+        //maillageBordImp = CréerBordureImpair(sommets);
+        //GetComponent<MeshFilter>().mesh = maillageBordImp;
+        //meshcBordImp.sharedMesh = maillageBordImp;
+        //GetComponent<MeshRenderer>().material = matériauxBordImp;
+
         //int textureRepeat = Mathf.RoundToInt(tiling * pointsSpline.Count); 
         //GetComponent<MeshRenderer>().sharedMaterial.mainTextureScale = new Vector2(1, textureRepeat);
     }
 
-    private Mesh CréerMeshRoute(List<Vector3> pointsSpline) // prend vector3 pour faire route et s'assure de fermer la route
+    
+
+    private Mesh CréerMeshRoute(List<Vector3> pointsSpline) 
     {
+        // le code CréerMeshRoute vient de https://www.youtube.com/watch?v=Q12sb-sOhdI
+
         sommets = new Vector3[pointsSpline.Count * 2];
-        Vector2[] uvs = new Vector2[sommets.Length]; // texture route
+        Vector2[] uvs = new Vector2[sommets.Length];
         int nbTriangles = 2 * pointsSpline.Count;
         int[] triangle = new int[nbTriangles * 3];
         int indexSom = 0;
         int indexTri = 0;
 
-        for (int i = 0; i < pointsSpline.Count; i++) //pour chaque point on veut la direction forward et 2 sommets/point
+        for (int i = 0; i < pointsSpline.Count; i++)
         {
             directionAvant = Vector3.zero;
-            if (i < pointsSpline.Count - 1) // dernier point 
+            if (i < pointsSpline.Count - 1)
             {
                 directionAvant += pointsSpline[(i + 1) % pointsSpline.Count] - pointsSpline[i];
             }
-            if (i > 0) // premier point
+            if (i > 0)
             {
                 directionAvant += pointsSpline[i] - pointsSpline[(i - 1 + pointsSpline.Count) % pointsSpline.Count];
             }
             directionAvant.Normalize();
-            gauche = new Vector3(-directionAvant.z, 0, directionAvant.x);  
+            gauche = new Vector3(-directionAvant.z, 0, directionAvant.x);
 
             // calculer 2 sommets
             sommets[indexSom] = pointsSpline[i] + gauche * largeurRoute;
@@ -68,33 +104,210 @@ public class ScriptSpline : MonoBehaviour
             uvs[indexSom] = new Vector2(0, completePercent);
             uvs[indexSom + 1] = new Vector2(1, completePercent);
 
-            
-            //tous les pts sauf le dernier pour faire les triangles 
             if (i < pointsSpline.Count - 1)
             {
                 triangle[indexTri] = indexSom;
                 triangle[indexTri + 1] = (indexSom + 2) % sommets.Length;
                 triangle[indexTri + 2] = indexSom + 1;
 
-                triangle[indexTri + 3] = (indexSom + 1) ; //2
-                triangle[indexTri + 4] = (indexSom + 2) % sommets.Length; //3
-                triangle[indexTri + 5] = (indexSom + 3) % sommets.Length; //1
+                triangle[indexTri + 3] = (indexSom + 1);
+                triangle[indexTri + 4] = (indexSom + 2) % sommets.Length;
+                triangle[indexTri + 5] = (indexSom + 3) % sommets.Length;
             }
             indexSom += 2;
             indexTri += 6;
         }
+
+        //bordure pair
+        //Vector2[] uvsBordurePair = new Vector2[sommets.Length];
+        //int nbTrianglesPair = sommets.Length;
+        //int[] trianglePair = new int[nbTrianglesPair * 3];
+        //static bool EstPair(int nb) { return nb % 2 == 0; }
+        //Vector3[] pointsBordurePair = new Vector3[sommets.Length];
+        //int indexSomPair = 0;
+        //int indexTriPair = 0;
+
+        //// bordure pair
+        //for (int i = 0; i < sommets.Length; i++)
+        //{
+        //    if (EstPair(i))
+        //    {
+        //        pointsBordurePair[i] = new Vector3(sommets[i].x, 0, sommets[i].z);
+        //        pointsBordurePair[i + 1] = new Vector3(sommets[i].x, sommets[i].y + 5f, sommets[i].z);
+
+        //        float completePercent = i / (float)(sommets.Length - 1);
+        //        uvsBordurePair[indexSomPair] = new Vector2(0, completePercent);
+        //        uvsBordurePair[indexSomPair + 1] = new Vector2(1, completePercent);
+
+        //        if (i < sommets.Length - 1)
+        //        {
+        //            trianglePair[indexTriPair] = indexSomPair;
+        //            trianglePair[indexTriPair + 1] = (indexSomPair + 2) % pointsBordurePair.Length;
+        //            trianglePair[indexTriPair + 2] = indexSomPair + 1;
+
+        //            trianglePair[indexTriPair + 3] = (indexSomPair + 1);
+        //            trianglePair[indexTriPair + 4] = (indexSomPair + 2) % pointsBordurePair.Length;
+        //            trianglePair[indexTriPair + 5] = (indexSomPair + 3) % pointsBordurePair.Length;
+        //        }
+        //        indexSomPair += 2;
+        //        indexTriPair += 6;
+
+        //    }
+
+        //}
 
         /*for (int i = 0; i < sommets.Length; i++)
         {
             Instantiate(GetComponent<GestionnaireJeux>().obstalce1, sommets[i],
                 GetComponent<GestionnaireJeux>().obstalce1.transform.rotation);
         }*/
+
+        //Vector3[] sommetsMesh = new Vector3[sommets.Length + pointsBordurePair.Length];
+        //for (int i = 0; i < sommets.Length; i++)
+        //{
+        //    sommetsMesh[i] = sommets[i];
+        //}
+        //for (int i = 0; i < pointsBordurePair.Length; i++)
+        //{
+        //    sommetsMesh[sommets.Length + i] = pointsBordurePair[i];
+        //}
+
+
+        //int[] trianglesMesh = new int[triangle.Length + trianglePair.Length];
+        //for (int i = 0; i < triangle.Length; i++)
+        //{
+        //    trianglesMesh[i] = triangle[i];
+        //}
+        //for (int i = 0; i < trianglePair.Length; i++)
+        //{
+        //    trianglesMesh[triangle.Length + i] = trianglePair[i] + triangle.Length - 1;
+        //}
+
+        //Vector2[] uvsMesh = new Vector2[uvs.Length + uvsBordurePair.Length];
+        //for (int i = 0; i < uvs.Length; i++)
+        //{
+        //    uvsMesh[i] = uvs[i];
+        //}
+        //for (int i = 0; i < uvsBordurePair.Length; i++)
+        //{
+        //    uvsMesh[uvs.Length + i] = uvsBordurePair[i];
+        //}
+
+        Debug.Log(sommets.Length);
         Mesh mesh = new Mesh();
         mesh.vertices = sommets;
         mesh.triangles = triangle;
         mesh.uv = uvs;
         return mesh;
-
-       
     }
+
+    //private static int[] ConcatenerTableauxInt(int[] t1, int[] t2)
+    //{
+    //    int[] tableauConcatenés = new int[t1.Length + t2.Length];
+
+    //    // On copie le premier tableau
+    //    for (int i = 0; i < t1.Length; i++)
+    //    {
+    //        tableauConcatenés[i] = t1[i];
+    //    }
+
+    //    // On copie le deuxième tableau à la suite
+    //    for (int i = 0; i < t2.Length; i++)
+    //    {
+    //        // observez bien l'indice ici!
+    //        tableauConcatenés[t1.Length + i] = t2[i];
+    //    }
+    //    return tableauConcatenés;
+    //}
+
+    //private Mesh CréerBordurePair(Vector3[] sommets)
+    //{
+    //    //bordure pair
+    //    Vector2[] uvsBordure = new Vector2[sommets.Length];
+    //    int nbTrianglesPair = sommets.Length;
+    //    int[] trianglePair = new int[nbTrianglesPair * 3];
+    //    static bool EstPair(int nb) { return nb % 2 == 0; }
+    //    Vector3[] pointsBordurePair = new Vector3[sommets.Length];
+    //    int indexSom = 0;
+    //    int indexTri = 0;
+
+    //    // bordure pair
+    //    for (int i = 0; i < sommets.Length; i++)
+    //    {
+    //        if (EstPair(i))
+    //        {
+    //            pointsBordurePair[i] = new Vector3(sommets[i].x, 0, sommets[i].z);
+    //            pointsBordurePair[i + 1] = new Vector3(sommets[i].x, sommets[i].y + 5f, sommets[i].z);
+
+    //            float completePercent = i / (float)(sommets.Length - 1);
+    //            uvsBordure[indexSom] = new Vector2(0, completePercent);
+    //            uvsBordure[indexSom + 1] = new Vector2(1, completePercent);
+
+    //            if (i < sommets.Length - 1)
+    //            {
+    //                trianglePair[indexTri] = indexSom;
+    //                trianglePair[indexTri + 1] = (indexSom + 2) % pointsBordurePair.Length;
+    //                trianglePair[indexTri + 2] = indexSom + 1;
+
+    //                trianglePair[indexTri + 3] = (indexSom + 1);
+    //                trianglePair[indexTri + 4] = (indexSom + 2) % pointsBordurePair.Length;
+    //                trianglePair[indexTri + 5] = (indexSom + 3) % pointsBordurePair.Length;
+    //            }
+    //            indexSom += 2;
+    //            indexTri += 6;
+    //        }
+    //    }
+
+    //    //SubMeshDescriptor meshBordure = new SubMeshDescriptor();
+    //    Mesh meshBordure = new Mesh();
+    //    meshBordure.vertices = pointsBordurePair;
+    //    meshBordure.triangles = trianglePair;
+    //    meshBordure.uv = uvsBordure;
+    //    return meshBordure;
+    //}
+
+    //private Mesh CréerBordureImpair(Vector3[] sommets)
+    //{
+    //    //bordure impair
+    //    Vector2[] uvsBordure = new Vector2[sommets.Length];
+    //    int nbTrianglesImpair = sommets.Length;
+    //    int[] triangleImpair = new int[nbTrianglesImpair * 3];
+    //    static bool EstPair(int nb) { return nb % 2 == 0; }
+    //    Vector3[] pointsBordureImpair = new Vector3[sommets.Length];
+    //    int indexSom = 0;
+    //    int indexTri = 0;
+
+    //    // bordure impair
+    //    for (int i = 0; i < sommets.Length; i++)
+    //    {
+    //        if (!EstPair(i))
+    //        {
+    //            pointsBordureImpair[i] = new Vector3(sommets[i].x, 0, sommets[i].z);
+    //            pointsBordureImpair[i + 1] = new Vector3(sommets[i].x, sommets[i].y + 5f, sommets[i].z);
+
+    //            float completePercent = i / (float)(sommets.Length - 1);
+    //            uvsBordure[indexSom] = new Vector2(0, completePercent);
+    //            uvsBordure[indexSom + 1] = new Vector2(1, completePercent);
+
+    //            if (i < sommets.Length - 1)
+    //            {
+    //                triangleImpair[indexTri] = indexSom;
+    //                triangleImpair[indexTri + 1] = (indexSom + 2) % pointsBordureImpair.Length;
+    //                triangleImpair[indexTri + 2] = indexSom + 1;
+
+    //                triangleImpair[indexTri + 3] = (indexSom + 1);
+    //                triangleImpair[indexTri + 4] = (indexSom + 2) % pointsBordureImpair.Length;
+    //                triangleImpair[indexTri + 5] = (indexSom + 3) % pointsBordureImpair.Length;
+    //            }
+    //            indexSom += 2;
+    //            indexTri += 6;
+    //        }
+    //    }
+
+    //    Mesh meshBordure = new Mesh();
+    //    meshBordure.vertices = pointsBordureImpair;
+    //    meshBordure.triangles = triangleImpair;
+    //    meshBordure.uv = uvsBordure;
+    //    return meshBordure;
+    //}
 }
