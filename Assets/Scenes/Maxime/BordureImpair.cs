d3b1a1b2feb27346783e555708087ad30e601eab
Maxime Fortier <202163543@edu.clg.qc.ca>
2023-03-15 11:58:38 -0400

    Bordure Impair qui fonctionne pas

--- Assets/Scenes/Maxime/BordureImpair.cs
+++ Assets/Scenes/Maxime/BordureImpair.cs
@@ -29,17 +29,20 @@ public class BordureImpair : MonoBehaviour
     private Mesh AjouterBordure()
     {
         static bool EstPair(int nb) { return nb % 2 == 0; }
-        Vector2[] uvsBordureImpair = new Vector2[sommet.Length + 1];
-        Vector3[] pointsBordureImpair = new Vector3[sommet.Length + 1];
+        Vector2[] uvsBordureImpair = new Vector2[sommet.Length];
+        Vector3[] pointsBordureImpair = new Vector3[sommet.Length];
         int nbTrianglesImpair = sommet.Length;
         int[] triangleImpair = new int[nbTrianglesImpair * 3];
         int indexSomImpair = 0;
         int indexTriImpair = 0;
 
-        for (int i = 0; i < sommet.Length; i++)
+        for (int i = 1; i < sommet.Length; i++)
         {
             if (!EstPair(i))
             {
+
+                i--;
+
                 pointsBordureImpair[i] = new Vector3(sommet[i].x, 0, sommet[i].z);
                 pointsBordureImpair[i + 1] = new Vector3(sommet[i].x, sommet[i].y + 5f, sommet[i].z);
 
@@ -47,7 +50,6 @@ public class BordureImpair : MonoBehaviour
                 uvsBordureImpair[indexSomImpair] = new Vector2(0, completePercent);
                 uvsBordureImpair[indexSomImpair + 1] = new Vector2(1, completePercent);
 
-                
                 if (i < sommet.Length - 1)
                 {
                     triangleImpair[indexTriImpair] = indexSomImpair;
@@ -61,6 +63,8 @@ public class BordureImpair : MonoBehaviour
 
                 indexSomImpair += 2;
                 indexTriImpair += 6;
+
+                i++;
             }
         }
         Mesh mesh = new Mesh();
